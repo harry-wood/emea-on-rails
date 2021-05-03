6f821d7db41b7994cea9573ac560e549d9f0ab22
Ariel Caplan <ariel.caplan@cloudinary.com>
2021-05-03 12:50:43 +0300

    Support all browsers on line-clamp strategy

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -8,3 +8,9 @@ const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
 
 console.info("Bridgetown is loaded!")
+
+import { webkitLineClamp } from 'webkit-line-clamp';
+let items = document.getElementsByClassName('clamp-6-lines');
+for (var i = 0; i < items.length; i++) {
+  webkitLineClamp(items[i], 6);
+}
